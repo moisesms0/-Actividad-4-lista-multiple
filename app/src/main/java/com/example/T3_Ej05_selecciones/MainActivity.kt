c2073443087faabf1bb3e65ce00eeed0b5030e87
Moisés M <moiseselquimico@gmail.com>
2023-10-29 20:07:45 +0100

    ejercicio

--- app/src/main/java/com/example/T3_Ej05_selecciones/MainActivity.kt
+++ app/src/main/java/com/example/T3_Ej05_selecciones/MainActivity.kt
@@ -2,8 +2,8 @@ package com.example.T3_Ej05_selecciones
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ListView
 import android.widget.TextView
 
@@ -24,30 +24,38 @@ class MainActivity : AppCompatActivity() {
             "Zamora"
         )
 
-        //Enlazamos con los elementos de la interfaz
+        // Enlazamos con los elementos de la interfaz
         val ListViewProvincias = findViewById<ListView>(R.id.provinceListView)
-        val TextViewProvinciaSeleccionadad = findViewById<TextView>(R.id.selectedProvinceTextView)
+        val TextViewProvinciaSeleccionada = findViewById<TextView>(R.id.selectedProvinceTextView)
 
         //Declaramos un adaptador
         //Pasamos el contexto
         //El layout de la vista
         //El array que se va a mostrar
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, provincias)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, provincias)
         //val adapter = ArrayAdapter(this, R.layout.fila, provincias)
         ListViewProvincias.adapter = adapter
 
-        //Fijamos un escuchador
-        ListViewProvincias.onItemClickListener =
-            //Interfaz anidada para manejar eventos
-            //Primer _ : referencia al objeto que desencadenó el evento (no se va a usar)
-            //Segundo _: referencia a la vista del elemento de la lista que se ha hecho click (no se va a usar)
-            //position: posición del elmento de la lista donde se ha hecho click
-            //Último _: referencia al ID del elemento que se ha hecho clic
-            //Función que muestra el TextView utilizando la posición
-            AdapterView.OnItemClickListener { _, _, position, _ ->
-                val selectedProvince = provincias[position]
-                TextViewProvinciaSeleccionadad.text = "Provincia seleccionada: $selectedProvince"
-            }
-    }
+        // Usamos el modo de seleccion multiple
+        ListViewProvincias.choiceMode = ListView.CHOICE_MODE_MULTIPLE
 
+        // Listener para el boton de confirmacion
+        val confirmButton = findViewById<Button>(R.id.confirmButton)
+        confirmButton.setOnClickListener {
+            val provinciasSeleccionadas = mutableListOf<String>()
+            val provinciasChecked = ListViewProvincias.checkedItemPositions
+
+            for (i in 0 until ListViewProvincias.count) {
+                // Comprueba si la provincia esta seleccionada y la agrega al array
+                if (provinciasChecked[i]) {
+                    provinciasSeleccionadas.add(provincias[i])
+                }
+            }
+            if (provinciasSeleccionadas.size == 0){
+                TextViewProvinciaSeleccionada.text = ""
+            }else{
+                TextViewProvinciaSeleccionada.text = "Provincias seleccionadas: ${provinciasSeleccionadas.joinToString(", ")}"
+            }
+        }
+    }
 }
